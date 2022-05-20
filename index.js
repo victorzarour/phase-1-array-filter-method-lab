88f79af66efebf87191d65bd3a315b9c86b2b956
Victor <vx_zz@hotmail.com>
2022-05-20 10:33:55 -0400

    commit tree df729ebda55f0a260d121d3b65f2803bbf872631

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, name){
+    return array.filter(element => name.toUpperCase() == element.toUpperCase())
+  }
+
+function fuzzyMatch(array, name){
+    return array.filter(element => name.substring(0,2) === element.substring(0,2))
+}
+
+function matchName() {
+    array.filter(element => name === element)
+}
+
+
+function matchName(array, name) {
+    return array.filter(element => name === element.name)
+  }
